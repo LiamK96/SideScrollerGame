caf02fc1cf63a9d2f6d22fc1cea3d16f5d84e9f3
Liamk96 <liamk1356@gmail.com>
2022-06-02 14:57:16 +0900

    Animation Framework

---
+++ src/main/java/components/Frame.java
@@ -0,0 +1,13 @@
+package components;
+
+public class Frame {
+
+    public Sprite sprite;
+    public float frameTime;
+
+    public Frame(Sprite spr, float time){
+        this.sprite = spr;
+        this.frameTime = time;
+    }
+
+}
