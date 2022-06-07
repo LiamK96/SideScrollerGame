73cdd2986b600c7ab59c199c9fb4ad4724a505d7
Liamk96 <liamk1356@gmail.com>
2022-06-07 15:46:31 +0900

    Sounds added

--- src/main/java/scenes/LevelEditorSceneInitializer.java
+++ src/main/java/scenes/LevelEditorSceneInitializer.java
@@ -55,6 +55,31 @@ public class LevelEditorSceneInitializer extends SceneInitializer {
                         24,48,3,0));
         AssetPool.getTexture("assets/images/blendImage1.png");
 
+        AssetPool.addSound("assets/sounds/1-up.ogg", false);
+        AssetPool.addSound("assets/sounds/bowserfalls.ogg", false);
+        AssetPool.addSound("assets/sounds/bowserfire.ogg", false);
+        AssetPool.addSound("assets/sounds/break_block.ogg", false);
+        AssetPool.addSound("assets/sounds/bump.ogg", false);
+        AssetPool.addSound("assets/sounds/coin.ogg", false);
+        AssetPool.addSound("assets/sounds/fireball.ogg", false);
+        AssetPool.addSound("assets/sounds/fireworks.ogg", false);
+        AssetPool.addSound("assets/sounds/flagpole.ogg", false);
+        AssetPool.addSound("assets/sounds/gameover.ogg", false);
+        AssetPool.addSound("assets/sounds/invincible.ogg", false);
+        AssetPool.addSound("assets/sounds/jump-small.ogg", false);
+        AssetPool.addSound("assets/sounds/jump-super.ogg", false);
+        AssetPool.addSound("assets/sounds/kick.ogg", false);
+        AssetPool.addSound("assets/sounds/main-theme-overworld.ogg", true);
+        AssetPool.addSound("assets/sounds/mario_die.ogg", false);
+        AssetPool.addSound("assets/sounds/pipe.ogg", false);
+        AssetPool.addSound("assets/sounds/powerup.ogg", false);
+        AssetPool.addSound("assets/sounds/powerup_appears.ogg", false);
+        AssetPool.addSound("assets/sounds/stage_clear.ogg", false);
+        AssetPool.addSound("assets/sounds/stomp.ogg", false);
+        AssetPool.addSound("assets/sounds/vine.ogg", false);
+        AssetPool.addSound("assets/sounds/warning.ogg", false);
+        AssetPool.addSound("assets/sounds/world_clear.ogg", false);
+
         for (GameObject go : scene.getGameObjects()){
             if (go.getComponent(SpriteRenderer.class) != null){
                 SpriteRenderer spr = go.getComponent(SpriteRenderer.class);
@@ -174,6 +199,8 @@ public class LevelEditorSceneInitializer extends SceneInitializer {
 
             if (ImGui.beginTabItem("Sounds")){
                 Collection<Sound> sounds = AssetPool.getAllSounds();
+
+                ImVec2 windowSize= new ImVec2(ImGui.getWindowPos());
                 for (Sound sound : sounds){
                     File tmp = new File(sound.getFilepath());
                     if (ImGui.button(tmp.getName())){
