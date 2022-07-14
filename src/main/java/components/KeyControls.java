3ee7fcc0c784aca3e592c712ce1c0a296b120b16
Liamk96 <liamk1356@gmail.com>
2022-07-14 11:49:54 +0900

    GameObjects key control movement

--- src/main/java/components/KeyControls.java
+++ src/main/java/components/KeyControls.java
@@ -22,17 +22,17 @@ public class KeyControls extends Component{
 
     @Override
     public void editorUpdate(float dt) {
-        debounce -=dt;
+        debounce -= dt;
 
         if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL)
                 && KeyListener.keyBeginPress(GLFW_KEY_D)
                 && propertiesWindow.getActiveGameObject() != null
-                && debounce<=0) {
+                && debounce <= 0) {
             GameObject newObject = propertiesWindow.getActiveGameObject().copy();
             Window.getScene().addGameObjectToScene(newObject);
             newObject.transform.position.add(new Vector2f(Settings.gridWidth, 0.0f));
             propertiesWindow.setActiveGameObject(newObject);
-            if (newObject.getComponent(StateMachine.class) !=null){
+            if (newObject.getComponent(StateMachine.class) != null) {
                 newObject.getComponent(StateMachine.class).refreshTextures();
             }
 
@@ -40,28 +40,60 @@ public class KeyControls extends Component{
         } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_CONTROL)
                 && KeyListener.isKeyPressed(GLFW_KEY_D)
                 && !propertiesWindow.getActiveGameObjects().isEmpty()
-                && debounce <= 0){
+                && debounce <= 0) {
             List<GameObject> gameObjects = new ArrayList<>(propertiesWindow.getActiveGameObjects());
             propertiesWindow.resetActiveGameObject();
-            for (GameObject go : gameObjects){
+            for (GameObject go : gameObjects) {
                 GameObject copy = go.copy();
                 Window.getScene().addGameObjectToScene(copy);
                 propertiesWindow.addActiveGameObject(copy);
-                if (copy.getComponent(StateMachine.class) !=null){
+                if (copy.getComponent(StateMachine.class) != null) {
                     copy.getComponent(StateMachine.class).refreshTextures();
                 }
             }
             debounce = 0.5f;
-        } else if(KeyListener.keyBeginPress(GLFW_KEY_DELETE)
-                && debounce <= 0){
-            for (GameObject go : propertiesWindow.getActiveGameObjects()){
+        } else if (KeyListener.keyBeginPress(GLFW_KEY_DELETE)
+                && debounce <= 0) {
+            for (GameObject go : propertiesWindow.getActiveGameObjects()) {
                 go.destroy();
             }
-            if (propertiesWindow.getActiveGameObject() != null){
+            if (propertiesWindow.getActiveGameObject() != null) {
                 propertiesWindow.getActiveGameObject().destroy();
             }
             propertiesWindow.resetActiveGameObject();
 
+            debounce = debounceTime;
+        } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_SHIFT)
+                && KeyListener.isKeyPressed(GLFW_KEY_D)
+                && !propertiesWindow.getActiveGameObjects().isEmpty()
+                && debounce <= 0) {
+            for (GameObject go : propertiesWindow.getActiveGameObjects()) {
+                go.transform.position.x += 0.25f;
+            }
+            debounce = debounceTime;
+        } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_SHIFT)
+                && KeyListener.isKeyPressed(GLFW_KEY_A)
+                && !propertiesWindow.getActiveGameObjects().isEmpty()
+                && debounce <= 0) {
+            for (GameObject go : propertiesWindow.getActiveGameObjects()) {
+                go.transform.position.x -= 0.25f;
+            }
+            debounce = debounceTime;
+        } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_SHIFT)
+                && KeyListener.isKeyPressed(GLFW_KEY_W)
+                && !propertiesWindow.getActiveGameObjects().isEmpty()
+                && debounce <= 0) {
+            for (GameObject go : propertiesWindow.getActiveGameObjects()) {
+                go.transform.position.y += 0.25f;
+            }
+            debounce = debounceTime;
+        } else if (KeyListener.isKeyPressed(GLFW_KEY_LEFT_SHIFT)
+                && KeyListener.isKeyPressed(GLFW_KEY_S)
+                && !propertiesWindow.getActiveGameObjects().isEmpty()
+                && debounce <= 0) {
+            for (GameObject go : propertiesWindow.getActiveGameObjects()) {
+                go.transform.position.y -= 0.25f;
+            }
             debounce = debounceTime;
         }
     }
