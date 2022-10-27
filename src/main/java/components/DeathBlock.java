361f162e0685f313a7c209fafff97cdee5e69e2b
Liamk96 <liamk1356@gmail.com>
2022-10-27 10:18:21 +0900

    DeathBlock disappears in runtime

--- src/main/java/components/DeathBlock.java
+++ src/main/java/components/DeathBlock.java
@@ -1,11 +1,27 @@
 package components;
 
 import engine.GameObject;
+import engine.Window;
 import org.jbox2d.dynamics.contacts.Contact;
 import org.joml.Vector2f;
+import org.joml.Vector4f;
 
 public class DeathBlock extends Component{
 
+    @Override
+    public void start(){
+        if (Window.isGameRunning()){
+            SpriteRenderer spr = gameObject.getComponent(SpriteRenderer.class);
+            if (spr != null){
+                spr.setColor(new Vector4f(0.0f,0.0f,0.0f, 0.0f));
+            }
+        } else {
+            SpriteRenderer spr = gameObject.getComponent(SpriteRenderer.class);
+            if (spr != null){
+                spr.setColor(new Vector4f(1.0f,1.0f,1.0f, 1.0f));
+            }
+        }
+    }
 
     @Override
     public void preSolve(GameObject go, Contact contact, Vector2f contactNormal){
